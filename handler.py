1e9425c057ae055ff792641ba00cbfc03a856d7e
ivanntis <ivanntis@gmail.com>
2019-07-24 02:34:26 -0500

    Se ctualiza

--- handler.py
+++ handler.py
@@ -1,7 +1,33 @@
+import os
 import json
+import logging
+import copy
+import random
+import mysql.connector as sql
+import requests
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+user = os.environ['DATABASE_USER']
+password = os.environ['DATABASE_PASSWORD']
+host = os.environ['DATABASE_HOST']
+name = os.environ['DATABASE_NAME']
+# google_api_key = os.environ['GOOGLE_API_KEY']
 
 def hello(event, context):
+    print("ingreso")
+    #logger.info(user+" "+password+" "+host+" "+database_name)
+    try:
+        #conn = sql.connect(user="pulsedb", password="asdf1234", host="pulsedbid.cquegzdtu9b8.us-east-1.rds.amazonaws.com", database="pulsedbinst")
+        conn = sql.connect(user=user, password=password, host=host, database=name)
+        logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
+
+        conn.close()
+    except:
+        logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+
+
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
